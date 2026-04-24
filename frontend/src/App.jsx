43b52e1beae05ac7ba8770b3d07cea557ccdfde2
Simeky <92762741+Simeky@users.noreply.github.com>
2026-04-24 19:30:01 -0300

    Arrumando main branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import {
   BrowserRouter,
   Navigate,
@@ -9,8 +11,14 @@ import BemVindo from './pages/bemVindo/BemVindo.jsx';
 import Busca from './pages/busca/Busca.jsx';
 import Cadastro from './pages/cadastro/Cadastro.jsx';
 import Login from './pages/login/login.jsx';
+import MeusLivros from './pages/meusLivros/MeusLivros.jsx';
 
 function App() {
+  useEffect(() => {
+    const temaSalvo = localStorage.getItem('theme') || 'dark';
+    document.documentElement.setAttribute('data-bs-theme', temaSalvo);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -18,6 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/busca" element={<Busca />} />
+        <Route path="/meus-livros" element={<MeusLivros />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
