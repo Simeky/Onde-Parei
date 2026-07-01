9c0d025daa5e7a2c82e778895c1a15f46c6789ba
Simeky <guilherme.rayser@gmail.com>
2026-07-01 16:41:22 -0300

    att monitoramento de rotas

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const livrosRoutes = require('./routes/livros.routes');
 const usuariosRoutes = require('./routes/usuarios.routes');
+const promMid = require('express-prometheus-middleware');
 
 const app = express();
 const PORTA = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+}));
 
 app.use('/livros', livrosRoutes); 
 app.use('/usuarios', usuariosRoutes);
